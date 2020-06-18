7044db90a895b0062291e4670f2ec2df79cb6666
yinsh <765009618@qq.com>
2020-06-18 15:59:08 +0800

    项目原型

---
+++ hrm-support-parent/hrm-config-server-9696/src/main/java/cn/yinsh/hrm/ConfigServerApplication_9696.java
@@ -0,0 +1,13 @@
+package cn.yinsh.hrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication_9696 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication_9696.class,args);
+    }
+}
